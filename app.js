9dc323c67079a6115ce6c69e9b3072cf1edef8fc
Rei01 <nyamdavaa.nyamka@yahoo.com>
2024-06-19 23:16:30 +0800

    shoonii ali talaaraa buusniig zurgaar haruuldag bolson

--- app.js
+++ app.js
@@ -8,15 +8,43 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Шооны аль талаараа буусанг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-console.log("Шоо " + dice + " буулаа");
+console.log("Шоо " + diceNumber + " буулаа");
 
 // Программ эхлэхэд бэлтгэе
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
-document.querySelector(".dice").style.display = 'none';
+var diceDom = document.querySelector(".dice");
+
+diceDom.style.display = 'none';
+
+// Roll dice товчийг программчлая
+document.querySelector(".btn-roll").addEventListener('click', function(){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    diceDom.style.display = 'block';
+
+    // if(diceNumber === 1) {
+    //         document.querySelector(".dice").src = 'dice-1.png';
+    //     } else if(diceNumber === 2) {
+    //         document.querySelector(".dice").src = 'dice-2.png';
+    //     } else if(diceNumber === 3) {
+    //         document.querySelector(".dice").src = 'dice-3.png';
+    //     } else if(diceNumber === 4) {
+    //         document.querySelector(".dice").src = 'dice-4.png';
+    //     } else if(diceNumber === 5) {
+    //         document.querySelector(".dice").src = 'dice-5.png';
+    //     } else if(diceNumber === 6) {
+    //         document.querySelector(".dice").src = 'dice-6.png';
+    //     } else {
+    //         console.log('aldaa garlaa');
+    //     }
+    diceDom.src = "dice-" + diceNumber + ".png";
+
+    console.log('Шоо ' + diceNumber + ' буулаа');
+});
